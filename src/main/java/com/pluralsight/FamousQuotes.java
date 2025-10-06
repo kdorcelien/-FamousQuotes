cc72081b0126a61397f9d7d98bb3de5a9ca686b8
kdorcelien <kdorcelien.kd@gmail.com>
2025-10-06 11:05:19 -0400

    Final commit

--- src/main/java/com/pluralsight/FamousQuotes.java
+++ src/main/java/com/pluralsight/FamousQuotes.java
@@ -1,7 +1,39 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class FamousQuotes {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String reply = "yes";
+        while (reply.equalsIgnoreCase("yes"))
+            try {
+                String[] quotes = {
+                        "Be yourself; everyone else is already taken.",
+                        "The only way to do great work is to love what you do.",
+                        "In the middle of difficulty lies opportunity.",
+                        "Life is what happens when you're busy making other plans.",
+                        "The journey of a thousand miles begins with one step.",
+                        "You miss 100% of the shots you don't take.",
+                        "Whether you think you can or you can't, you're right.",
+                        "Stay hungry, stay foolish.",
+                        "Do or do not. There is no try.",
+                        "It always seems impossible until it's done.",
+                };
+                System.out.print("Please select a number \n between 1 and 10: ");
+                int choice = input.nextInt();
+                input.nextLine();
+                System.out.println(quotes[choice - 1]);
 
-    }
+                System.out.println("Would you like to see another quote: Yes or No");
+                 reply = input.nextLine();
+
+
+            } catch (Exception e) {
+                System.out.println("Selected number is out of reach, Please select between 1 and 10.");
+                System.out.println(e.getMessage());
+
+            }
+        }
+
 }
